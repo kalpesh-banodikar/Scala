b37c99a3f591eb163a8c51f20c0c2f0f8435b448
kalpesh-banodikar <37872440+kalpesh-banodikar@users.noreply.github.com>
2019-11-01 21:22:48 +0530

    Add files via upload

---
+++ Vars.scala
@@ -0,0 +1,11 @@
+object Vars extends App{
+    var I:Int =10
+    var D:Double=10.5
+    var C:Char='A'
+    var S:String="Hello"
+    val B:Boolean=true
+    val b:Byte = 100
+    val s:Short = 32000
+    val l:Long =2000000000
+    print(I,D,C,S,B,b,s,l)
+}
